9c1aeb623ea0d398f3d65009f8d739a55ebda7c4
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-10 00:07:11 -0400

    Setting View Data in the HomeController.cs File in the Controllers Folder

--- PartyInvite/Controllers/HomeController.cs
+++ PartyInvite/Controllers/HomeController.cs
@@ -7,15 +7,15 @@ using Microsoft.AspNetCore.Mvc;
 using PartyInvite.Models;
 
 namespace PartyInvite.Controllers {
+
     public class HomeController : Controller
     {
-        /* When I return a ViewResult object from an action method, I am instructing MVC to render a view. I
-        create the ViewResult object by calling the View method, specifying the name of the view that I want to use,
-        which is MyView.If you run the application, you can see MVC trying to find the view, as shown in the error
-        message displayed
-        */
-       public ViewResult Index(){
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
+        
         }
     }
 }
