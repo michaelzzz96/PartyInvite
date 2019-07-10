0260b1fa774cf3e8ce0740be8112a99e2a52dc4f
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-09 23:26:34 -0400

    Adding the Controller

--- PartyInvite/Controllers/HomeController.cs
+++ PartyInvite/Controllers/HomeController.cs
@@ -6,32 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PartyInvite.Models;
 
-namespace PartyInvite.Controllers
-{
+namespace PartyInvite.Controllers {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+       public string Index(){
+            return "Hello World";
         }
     }
 }
