939a8239697675887b1eb8cae09ab8391fea8e06
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-09 23:45:22 -0400

    Creating and Rendering a View

--- PartyInvite/Controllers/HomeController.cs
+++ PartyInvite/Controllers/HomeController.cs
@@ -9,8 +9,13 @@ using PartyInvite.Models;
 namespace PartyInvite.Controllers {
     public class HomeController : Controller
     {
-       public string Index(){
-            return "Hello World";
+        /* When I return a ViewResult object from an action method, I am instructing MVC to render a view. I
+        create the ViewResult object by calling the View method, specifying the name of the view that I want to use,
+        which is MyView.If you run the application, you can see MVC trying to find the view, as shown in the error
+        message displayed
+        */
+       public ViewResult Index(){
+            return View("MyView");
         }
     }
 }
