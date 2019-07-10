6658a46482f84a467a32bbdd1728b7fa2af8c73b
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-10 00:34:51 -0400

    Contents of the GuestResponse.cs File in the Models Folder

--- PartyInvite/Models/GuestResponse.cs
+++ PartyInvite/Models/GuestResponse.cs
@@ -7,5 +7,9 @@ namespace PartyInvite.Models
 {
     public class GuestResponse
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
     }
 }
