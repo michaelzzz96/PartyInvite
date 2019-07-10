136628897c645994cdfe89b755165ddf44b59b0f
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-10 01:27:05 -0400

    Added Content to the Repository Class.

--- PartyInvite/Models/Repository.cs
+++ PartyInvite/Models/Repository.cs
@@ -5,7 +5,21 @@ using System.Threading.Tasks;
 
 namespace PartyInvite.Models
 {
-    public class Repository
+    // Set to static, which will make it easy for me to store and retrieve data from different places in the application.
+    public static class Repository
     {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
     }
 }
