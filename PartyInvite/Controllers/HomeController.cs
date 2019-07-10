b85bdbfe745d7d7c42a7daa2990cdb8de32a19b4
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-10 02:36:41 -0400

    Added style to the RSVP Form, Thanks and List Form

--- PartyInvite/Controllers/HomeController.cs
+++ PartyInvite/Controllers/HomeController.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using PartyInvite.Models;
+using PartyInvites.Models;
+using System.Linq;
 
-namespace PartyInvite.Controllers {
+namespace PartyInvites.Controllers
+{
 
     public class HomeController : Controller
     {
+
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
--- PartyInvite/Models/Repository.cs
+++ PartyInvite/Models/Repository.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace PartyInvite.Models
+namespace PartyInvites.Models
 {
-    // Set to static, which will make it easy for me to store and retrieve data from different places in the application.
     public static class Repository
     {
         private static List<GuestResponse> responses = new List<GuestResponse>();
@@ -17,6 +13,7 @@ namespace PartyInvite.Models
                 return responses;
             }
         }
+
         public static void AddResponse(GuestResponse response)
         {
             responses.Add(response);
